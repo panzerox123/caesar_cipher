cdf1e9c91e29790b4bed2838856a12aebeb5a859
NoobCoder14 <51149359+NoobCoder14@users.noreply.github.com>
2020-03-29 18:47:44 +0530

    Update cypher.c
    Added in the uppercase and lowercase distinction

--- cypher.c
+++ cypher.c
@@ -6,9 +6,11 @@ char * encrypt(char *, int);
 char * decrypt(char *, int);
 
 int main(){
-    char test[] = "BIGOTED";
-    char test2[] = "M KS XS EDOEFER";
+    char test[50], test2[50];
+    printf("Enter the word to be Caesar'd\n");
+    scanf("%s", test);
     printf("%s\n",encrypt(test,5));
+    scanf("%s", test2);
     printf("%s\n",decrypt(test2,4));
     return 0;
 }
@@ -20,9 +22,17 @@ char * encrypt(char *message, int shift_key){
             index++;
             continue;
         }
-        if(*index + shift_key > 'Z'){
-            *index = *index + shift_key-26;
-        } else *index = *index+shift_key;
+        if(*index>='A' && *index <='Z'){
+            if(*index + shift_key > 'Z')
+                *index = *index + shift_key-26;
+            else *index = *index+shift_key;
+        }
+        if(*index>='a'&&*index<='z'){
+            if(*index + shift_key > 'z')
+                *index = *index + shift_key-26;
+            else *index = *index+shift_key;
+        }
+
         index++;
     }
     return message;
@@ -35,12 +45,19 @@ char * decrypt(char *message, int shift_key){
             index++;
             continue;
         }
-        if(*index - shift_key < 'A'){
-            *index = *index - shift_key+26;
-        } else *index = *index-shift_key;
+        if(*index>= 'A' && *index <= 'Z'){
+            if(*index - shift_key < 'A')
+                *index = *index - shift_key+26;
+            else
+                *index = *index-shift_key;
+        }
+
+        if(*index >='a' && *index <='z'){
+            if(*index - shift_key < 'a')
+                *index = *index - shift_key+26;
+            else *index = *index-shift_key;
+        }
         index++;
     }
     return message;
 }
-
-
